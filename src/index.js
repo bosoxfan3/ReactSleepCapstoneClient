d7c664797496efb481f75258c41ef221e15d17d7
Daniel Acquesta <danielacquesta@sbcglobal.net>
2017-12-04 10:35:16 -0800

    installed packages and updated index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import App from './components/app';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, 
+  <Provider >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
