e67b6a04d80778ea7c31cf560b35044c9676cb4d
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-01-04 14:54:07 -0800

    forms are working again

--- src/components/form-components/radio.js
+++ src/components/form-components/radio.js
@@ -8,19 +8,18 @@ export default function Radio(props) {
   const input=props.input;
   const options = props.options.map((option, index) => (
     <div className="inline-block" key={index}>
-
-      <label htmlFor={name} key={index}>
-      <span className="radio-option"></span>
-        {option}
-      </label>
       <Field
         component="input"
         value={option}
         id={name+index}
         type="radio"
         name={name}
-        onClick={event => console.log("asd")}
       />
+      <label htmlFor={name} key={index}>
+      <span className="radio-option"></span>
+        {option}
+      </label>
+
     </div>
   ));
   return (
