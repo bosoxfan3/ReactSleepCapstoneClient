764c42097462b406e6571b00d5c44f8a015bd80c
Sebastian Bastidas <sbastidasr@gmail.com>
2018-01-04 17:06:15 -0500

    fixes

--- src/components/form-components/radio.js
+++ src/components/form-components/radio.js
@@ -5,19 +5,22 @@ import './radio.css';
 
 export default function Radio(props) {
   const name=props.input.name;
+  const input=props.input;
   const options = props.options.map((option, index) => (
     <div className="inline-block" key={index}>
+
+      <label htmlFor={name} key={index}>
+      <span className="radio-option"></span>
+        {option}
+      </label>
       <Field
         component="input"
         value={option}
         id={name+index}
         type="radio"
         name={name}
+        onClick={event => console.log("asd")}
       />
-      <label htmlFor={name} key={index}>
-      <span className="radio-option"></span>
-        {option}
-      </label>   
     </div>
   ));
   return (
@@ -26,3 +29,4 @@ export default function Radio(props) {
     </div>
   );
 }
+//    onChange={event => input.onChange(option)}
