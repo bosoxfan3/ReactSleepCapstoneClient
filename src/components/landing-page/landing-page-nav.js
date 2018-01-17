6839e3fe75a3adb567cd4a366e7db08967742032
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-01-16 22:31:58 -0800

    added demo link and functionality to landing page nav

--- src/components/landing-page/landing-page-nav.js
+++ src/components/landing-page/landing-page-nav.js
@@ -2,21 +2,29 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 
+import {login} from '../../actions/auth';
+
 import './landing-page-nav.css';
 
-export function LandingPageNav() {
-  return (
-    <header>
-      <nav className="landing-page-nav">
-        <Link className="main-link" to="/">RestFull</Link>
-        <img className="logo" src="https://image.flaticon.com/icons/svg/209/209714.svg" alt="moon logo" />
-        <div className="login-signup-links">
-          <Link className="link" to="/signup">Sign Up</Link>
-          <Link className="link" to="/login">Log In</Link>
-        </div>
-      </nav>
-    </header>
-  );
+export class LandingPageNav extends React.Component {
+  logInDemoAccount() {
+    return this.props.dispatch(login('daniel', '12345'));
+  }
+  render() {
+    return (
+      <header>
+        <nav className="landing-page-nav">
+          <Link className="main-link" to="/">RestFull</Link>
+          <img className="logo" src="https://image.flaticon.com/icons/svg/209/209714.svg" alt="moon logo" />
+          <div className="login-signup-links">
+            <Link className="link" to="/signup">Sign Up</Link>
+            <Link className="link" to="/login">Log In</Link>
+            <a className="link" onClick={() => this.logInDemoAccount()}>Demo</a>
+          </div>
+        </nav>
+      </header>
+    );
+  }
 }
 
 export default connect()(LandingPageNav);
