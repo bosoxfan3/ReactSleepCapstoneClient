9759d61ab3084a6324739876b1b4f8eaa50b0e48
Daniel Acquesta <danielacquesta@sbcglobal.net>
2018-01-16 21:51:45 -0800

    all basic testing complete. smoke tests, sleep-actions, sleep-reducers, and basic props and state tests

--- src/components/stats-charts/average-hours.test.js
+++ src/components/stats-charts/average-hours.test.js
@@ -4,7 +4,25 @@ import {AverageHours} from './average-hours';
 import {shallow} from 'enzyme';
 
 describe('<AverageHours />', () => {
+  const sleep1 = {
+    awakeTime: "22:00",
+    alarm: false,
+    bedTime:  "08:00",
+    blueLight:  false,
+    caffeine:  10,
+    date:  "Jan 01 2017",
+    exercise:  false,
+    hours:  10,
+    id:  "123",
+    moodAtSleep:  5,
+    moodAtWake: 5
+  };
   it('renders without crashing', () => {
     shallow(<AverageHours sleeps={[]}/>);
+  });
+  it('should calculate average hours', () => {
+    const wrapper = shallow(<AverageHours sleeps={[sleep1]} />);
+    const average = wrapper.find('#average-hours-div span');
+    expect(average.text()).toEqual('10.00');
   });
 });
--- src/components/stats-charts/percentage-bar.test.js
+++ src/components/stats-charts/percentage-bar.test.js
@@ -4,7 +4,25 @@ import {PercentageBar} from './percentage-bar';
 import {shallow} from 'enzyme';
 
 describe('<PercentageBar />', () => {
+  const sleep1 = {
+    awakeTime: "22:00",
+    alarm: false,
+    bedTime:  "08:00",
+    blueLight:  false,
+    caffeine:  10,
+    date:  "Jan 01 2017",
+    exercise:  false,
+    hours:  10,
+    id:  "123",
+    moodAtSleep:  5,
+    moodAtWake: 6
+  };
   it('renders without crashing', () => {
     shallow(<PercentageBar sleeps={[]}/>);
+  });
+  it('should render the positive sleep percentage', () => {
+    const wrapper = shallow(<PercentageBar sleeps={[sleep1]} />);
+    const positivePercentage = wrapper.find('.percentage-bar h4');
+    expect(positivePercentage.text()).toEqual('100.00%');
   });
 });
